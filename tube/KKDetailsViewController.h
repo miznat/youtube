36bc89303a69bbe88f2e392412cc57dfaa215a73
robertmryan <robert.ryan@mindspring.com>
2013-02-07 08:50:51 -0500

    Add details view controller

---
+++ tube/KKDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  KKDetailsViewController.h
+//  tube
+//
+//  Created by Robert Ryan on 2/7/13.
+//  Copyright (c) 2013 ikhlas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KKDetailsViewController : UIViewController
+
+@end
---
+++ tube/KKDetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  KKDetailsViewController.m
+//  tube
+//
+//  Created by Robert Ryan on 2/7/13.
+//  Copyright (c) 2013 ikhlas. All rights reserved.
+//
+
+#import "KKDetailsViewController.h"
+
+@interface KKDetailsViewController ()
+
+@end
+
+@implementation KKDetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
